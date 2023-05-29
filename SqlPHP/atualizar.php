5e0a2e04eed7e0bdb98f2e5ffecb6faf941dbbed
Vini_Beri <99839128+ViniciusVChabariberi@users.noreply.github.com>
2023-05-29 15:30:08 -0300

    Update atualizar.php

--- SqlPHP/atualizar.php
+++ SqlPHP/atualizar.php
@@ -21,41 +21,13 @@
     }
 
     $id = (int) $_GET['id'];
-?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/reset.css">
-  </head>
-    <body class="body--atualizar">
-        <div class="container">
-            <div class="row">
-            <div class="col">
-                <nav class="navbar rosa">
-                    <a href="consulta.php" class="navbar-brand title"><p class="p">Consulta de Dados</p></a>
-                        <a href="index.php" class="navbar-brand"><h3 class="title">SISTEMA WEB</h3></a>
-                    <a href="cadastroPag.php" class="navbar-brand title"><p class="p">Cadastro de Clientes</p></a>
-                </nav>
-            </div>    
-            </div>
-            <div class="row">
-                <div class="col">
-                <p class="subtitle--atualizar">Parabéns, seu cadastro foi atualizado!</p>
-                <br>
-                <br>
-                <br>
-                <br>
-                <a href="index.php" class="Options2--atualizar"><-- Voltar </a>
-                <a href="consulta.php" class="Options2--atualizar"> Consultar os dados --></a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>            
+if ($resultDelete !== false) {
+
+    header("Location: consulta.php");
+    exit();
+} else {
+
+    echo "Erro ao excluir os dados.";
+}
+?>
